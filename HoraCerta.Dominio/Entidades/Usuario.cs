44a87e41f566028bfda9d1a71cce8db732a78b71
Marcus vinicius <mcs-vinicius@hotmail.com>
2016-11-16 22:19:20 -0200

    Estrutura
    Estrutura do software utilizando DDD + MVC

---
+++ HoraCerta.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoraCerta.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int codUsuario { get; set; }
+        public  string Nome { get; set; }
+        public  string usuario { get; set; }
+        public  string senha { get; set; }
+        public DateTime dataCadastro { get; set; }
+
+    }
+}
---
+++ HoraCerta.Infra.Data/Contexto/HoraCertaContexto.cs
@@ -0,0 +1,16 @@
+﻿using HoraCerta.Dominio.Entidades;
+using System.Data.Entity;
+
+namespace HoraCerta.Infra.Data.Contexto
+{
+    public class HoraCertaContexto:DbContext
+    {
+        public HoraCertaContexto()
+            :base("HoraCerta")
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
